b34200b7b6d2081b844ce6b7d2821905d36904ce
karan jain <karanjain2212@gmail.com>
2023-01-05 10:43:58 +0530

    modified functions added modules

--- src/index.js
+++ src/index.js
@@ -1,54 +1,27 @@
 import "./style.css";
+import { getData, sendData } from "./modules/API";
+import Leaderboard from "./modules/leaderboard";
+
 const refreshBtn = document.querySelector(".refresh");
 const submitBtn = document.querySelector("#submit");
 const nameOfPlayer = document.querySelector("#nameInput");
 const scoreOfPlayer = document.querySelector("#scoreInput");
 
-//  POST request using fetch()
-const sendData = async () => {
+//adding eventlistner on both button
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   let name = nameOfPlayer.value;
   let score = scoreOfPlayer.value;
-  console.log(name, score);
-  fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ObQuEWVhBtSBKHyny4Vv/scores",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        user: name,
-        score: score,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-};
+  let list = new Leaderboard(name, score);
+  sendData(list);
+  nameOfPlayer.value = "";
+  scoreOfPlayer.value = "";
+});
 
-submitBtn.addEventListener("click", sendData);
+refreshBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getData();
+});
 
-//  GET request using fetch()
-
-const getData = async () => {
-  let response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ObQuEWVhBtSBKHyny4Vv/scores"
-  );
-
-  let data = await response.json();
-  console.log(data);
-  // Create a variable to store HTML
-  let li = ``;
-
-  // Loop through each data and add a table row
-  data.result.forEach((users) => {
-    li += `  <li>
-    <span class="name">${users.user}</span>: <span class="score">${users.score}</span>
-  </li>`;
-  });
-
-  // Display result
-  document.querySelector(".list").innerHTML = li;
-};
-
-refreshBtn.addEventListener("click", getData);
+//pageload event
+window.addEventListener("DOMContentLoaded", getData);
